daa724d0872925d4fa36fa7c83e1fcf5de293d3b
neshvig10 <svigneshwaran1073@gmail.com>
2023-10-25 18:30:23 +0530

    forms frontend

--- src/main/java/com/Snigdha/Snigdha/SnigdhaApplication.java
+++ src/main/java/com/Snigdha/Snigdha/SnigdhaApplication.java
@@ -2,8 +2,9 @@ package com.Snigdha.Snigdha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JdbcTemplateAutoConfiguration.class})
 public class SnigdhaApplication {
 
 	public static void main(String[] args) {
